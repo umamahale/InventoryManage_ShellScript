526b2b6b3bc7dae0692eff9357f607b8036309d9
umamahale <umagasmos@gmail.com>
2021-10-04 00:18:14 -0400

    first message

---
+++ StockManagement.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagement1
+{
+    class StockManagement
+    {
+        private string name;
+        private int code;
+        private int rate;
+        private int stock;
+        private int lead;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Code
+        {
+            get
+            {
+                return code;
+            }
+
+            set
+            {
+                code = value;
+            }
+        }
+
+        public int Rate
+        {
+            get
+            {
+                return rate;
+            }
+
+            set
+            {
+                rate = value;
+            }
+        }
+
+        public int Stock
+        {
+            get
+            {
+                return stock;
+            }
+
+            set
+            {
+                stock = value;
+            }
+        }
+
+        public int Lead
+        {
+            get
+            {
+                return lead;
+            }
+
+            set
+            {
+                lead = value;
+            }
+        }
+
+        public StockManagement(string name, int code, int rate, int stock, int lead)
+        {
+            this.name = name;
+            this.code = code;
+            this.rate = rate;
+            this.stock = stock;
+            this.lead = lead;
+        }
+    }
+}
